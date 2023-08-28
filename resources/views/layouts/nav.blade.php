c537686b06498038cc59b0ea3dc630b384367f07
Wulan <wulansandhi@gmail.com>
2023-08-28 15:39:05 +0700

    fe

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,36 +1,42 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white">
     <div class="container-fluid">
-      <a class="navbar-brand" href="{{url('/')}}"><img class="img-thumbnail" src="{{Vite::asset('resources/images/logo.png') }}" alt="logo petrokopindo"></a>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="{{url('/')}}">Beranda</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Arsip</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi-person-circle me-1"></i> Administrator</a>
+        <a class="navbar-brand" href="{{ url('/') }}"><img class="img-thumbnail"
+                src="{{ Vite::asset('resources/images/logo.png') }}" alt="logo petrokopindo"></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{ url('/') }}">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Arsip</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false"><i class="bi-person-circle me-1"></i>
+                        Administrator</a>
                     {{-- {{ Auth::user()->name }} --}}
 
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> My Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li>
-                        <a class="dropdown-item text-danger" href="#"
-                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            <i class="bi bi-lock-fill"></i> {{ __('Logout') }}
-                        </a>
-                        <form id="logout-form" action="#" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-      </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> My Profile</a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item text-danger" href="#"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <i class="bi bi-lock-fill"></i> {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="#" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav>
